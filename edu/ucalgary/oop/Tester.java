e863a7fe51a86c9ffd05ac6b004ec1cb1cf2fe64
chantaeh <chantae.ho@ucalgary.ca>
2023-03-12 12:15:06 -0600

    Added Nocturnal getFeedingHours test

--- edu/ucalgary/oop/Tester.java
+++ edu/ucalgary/oop/Tester.java
@@ -11,4 +11,17 @@ import org.junit.*;
 
 public class Tester {
 
+    /**
+     * Tests if Nocturnal class's static getter method works
+     */
+    @Test
+    public void testNocturnalGetFeedingHours() {
+        int[] expectedNocturnalHours = {0, 1, 2};
+        int[] actualNocturnalHours = Nocturnal.getFeedingHours();
+
+        for (int i = 0; i < expectedNocturnalHours.length; i++) {
+            assertEquals(expectedNocturnalHours[i], actualNocturnalHours[i]);
+        }
+    } 
+
 }
