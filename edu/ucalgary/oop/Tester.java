6d9bd6bd9d5ce5e2508a3c9ffa29c1da86484c12
chantaeh <chantae.ho@ucalgary.ca>
2023-03-12 11:29:15 -0600

    Initial commit

---
+++ edu/ucalgary/oop/Tester.java
@@ -0,0 +1,14 @@
+/**
+ * JUnit Tester class
+ * @author Group 25
+ */
+
+package edu.ucalgary.oop;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Tester {
+
+}
