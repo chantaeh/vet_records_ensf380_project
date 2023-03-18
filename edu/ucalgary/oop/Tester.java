4c2a89d3259daa8b33bcce590645cf0ccbbc22bc
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2023-03-18 17:57:08 -0600

    Task test

--- edu/ucalgary/oop/Tester.java
+++ edu/ucalgary/oop/Tester.java
@@ -10,5 +10,39 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class Tester {
+    String description = "description";
+    int duration = 1;
+    int maxWindow = 2;
+    int startHour = 3;
+    
+    @Test
+    public void testTaskCostrctor(){
+        Task task = new Task();
+        assertNotNull("Object was not created by Task when correct data was given ", task);
+    }
+
+    @Test
+    public void testGetDescription(){
+        Task task = new Task(description, duration, maxWindow, startHour);
+        assertEquals("Description was not returned correctly", description, task.getDescription());
+    }
+
+    @Test
+    public void testGetDuration(){
+        Task task = new Task(description, duration, maxWindow, startHour);
+        assertEquals("Duration was not returned correctly", duration, task.getDuration());
+    }
+
+    @Test
+    public void testGetMaxWindow(){
+        Task task = new Task(description, duration, maxWindow, startHour);
+        assertEquals("MaxWindow was not returned correctly", maxWindow, task.getMaxWindow());
+    }
+
+    @Test
+    public void testGetStartHour(){
+        Task task = new Task(description, duration, maxWindow, startHour);
+        assertEquals("StartHour was not returned correctly", startHour, task.getStartHour());
+    }
 
 }
