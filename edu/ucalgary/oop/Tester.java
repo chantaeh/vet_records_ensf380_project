9671724c4d1b3b8817c4d6550c2dc6d8e6f5ddd3
Andrew Yan <122841883+andrewy94@users.noreply.github.com>
2023-03-19 11:59:14 -0600

    tests for animals set times

--- edu/ucalgary/oop/Tester.java
+++ edu/ucalgary/oop/Tester.java
@@ -10,5 +10,103 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class Tester {
+      
+    /*fox tests */
 
+    @Test
+    public void testGetFeedMins() {
+        Fox fox = new Fox();
+        assertEquals(5, fox.getFeedMins());
+    }
+
+    @Test
+    public void testGetFeedinPrepMins() {
+        Fox fox = new Fox();
+        assertEquals(5, fox.getFeedinPrepMins());
+    }
+
+    @Test
+    public void testGetCageCleanMins() {
+        Fox fox = new Fox();
+        assertEquals(5, fox.getCageCleanMins());
+    }
+
+    /*trash panda tests */
+
+    @Test
+    public void testGetFeedMins() {
+        Raccoon raccoon = new Raccoon();
+        assertEquals(5, racoon.getFeedMins());
+    }
+
+    @Test
+    public void testGetFeedinPrepMins() {
+        Raccoon raccoon = new Raccoon();
+        assertEquals(0, raccoon.getFeedinPrepMins());
+    }
+
+    @Test
+    public void testGetCageCleanMins() {
+        Raccoon raccoon = new Raccoon();
+        assertEquals(5, raccoon.getCageCleanMins());
+    }    
+
+    /*beaver tests */
+
+    @Test
+    public void testGetFeedMins() {
+        Beaver beaver = new Beaver();
+        assertEquals(5, beaver.getFeedMins());
+    }
+
+    @Test
+    public void testGetFeedinPrepMins() {
+        Beaver beaver = new Beaver();
+        assertEquals(0, beaver.getFeedinPrepMins());
+    }
+
+    @Test
+    public void testGetCageCleanMins() {
+        Beaver beaver = new Beaver();
+        assertEquals(5, beaver.getCageCleanMins());
+    }    
+
+    /*porcupine tests */
+
+    @Test
+    public void testGetFeedMins() {
+        Porcupine porcupine = new Porcupine();
+        assertEquals(5, porcupine.getFeedMins());
+    }
+
+    @Test
+    public void testGetFeedinPrepMins() {
+        Porcupine porcupine = new Porcupine();
+        assertEquals(0, porcupine.getFeedinPrepMins());
+    }
+
+    @Test
+    public void testGetCageCleanMins() {
+        Porcupine porcupine = new Porcupine();
+        assertEquals(10, porcupine.getCageCleanMins());
+    }
+    /*coyote tests */ 
+
+    @Test
+    public void testGetFeedMins() {
+        Coyote coyote = new Coyote();
+        assertEquals(5, coyote.getFeedMins());
+    }
+
+    @Test
+    public void testGetFeedinPrepMins() {
+        Coyote coyote = new Coyote();
+        assertEquals(10, coyote.getFeedinPrepMins());
+    }
+
+    @Test
+    public void testGetCageCleanMins() {
+        Coyote coyote = new Coyote();
+        assertEquals(5, coyote.getCageCleanMins());
+    }              
 }
