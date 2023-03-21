6d4be126482ecad47eb7a4cd473139cfc0a48aef
Andrew Yan <122841883+andrewy94@users.noreply.github.com>
2023-03-21 10:53:43 -0600

    exceptions

--- edu/ucalgary/oop/Tester.java
+++ edu/ucalgary/oop/Tester.java
@@ -34,6 +34,8 @@ public class Tester {
         assertEquals("Fox Constructor does not return 'name' correctly", name, fox.getName());
         assertEquals("Fox Constructor does not return 'numAnimals' correctly", numAnimals, fox.getNumAnimals());
         }
+
+    
     /*test fox methods */
     @Test
     public void testFoxGetFeedMins() {
@@ -53,7 +55,7 @@ public class Tester {
         assertEquals("Fox method GetCageCleanMins() does not return correct time.", 5, fox.getCageCleanMins());
     }
 
-    /*trash panda tests */
+    /* raccoon tests */
     /*test getFeedingHours inherited from nocturnal*/
     @Test
     public void testRaccoonGetFeedingHours() {
@@ -219,5 +221,132 @@ public class Tester {
     public void testCoyoteGetCageCleanMins() {
         Coyote coyote = new Coyote("Coyote", 1);
         assertEquals("Coyote method GetCageCleanMins() does not return correct time.", 5, coyote.getCageCleanMins());
-    }              
+    }   
+    
+    /*TESTS FOR ILLEGALARGUMENTEXCEPTION OF THE CONSTRUCTORS FOR FOX, RACCOON, ETC. */
+    /*Fox constructor test for illegalargumentexception*/
+    @Test
+    public void testFoxConstructorInvalidData() {
+        boolean exceptionThrown = false;
+
+        // Test with negative numAnimals
+        try {
+            Fox fox = new Fox("Fox", -1);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when numAnimals was negative", exceptionThrown);
+        exceptionThrown = false;
+
+        // Test with null name
+        try {
+            Fox fox = new Fox(null, 1);
+        } catch (IllegalArgumentException e) {
+        exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when name was null", exceptionThrown);
+        exceptionThrown = false;
+    }
+        
+
+    /*Raccoon constructor test for illegalargumentexception*/
+    @Test
+    public void testRaccoonConstructorInvalidData() {
+        boolean exceptionThrown = false;
+
+        // Test with negative numAnimals
+        try {
+            Raccoon raccoon = new Raccoon("Raccoon", -1);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when numAnimals was negative", exceptionThrown);
+        exceptionThrown = false;
+
+        // Test with null name
+        try {
+            Raccoon raccoon = new Raccoon(null, 1);
+        } catch (IllegalArgumentException e) {
+        exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when name was null", exceptionThrown);
+        exceptionThrown = false;
+    }
+
+    /*Beaver constructor test for illegalargumentexception*/
+    @Test
+    public void testBeaverConstructorInvalidData() {
+        boolean exceptionThrown = false;
+
+        // Test with negative numAnimals
+        try {
+            Beaver beaver = new Beaver("Beaver", -1);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when numAnimals was negative", exceptionThrown);
+        exceptionThrown = false;
+
+        // Test with null name
+        try {
+            Beaver beaver = new Beaver(null, 1);
+        } catch (IllegalArgumentException e) {
+        exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when name was null", exceptionThrown);
+        exceptionThrown = false;
+    }
+
+    /*Porcupine constructor test for illegalargumentexception*/
+    @Test
+    public void testPorcupineConstructorInvalidData() {
+        boolean exceptionThrown = false;
+
+        // Test with negative numAnimals
+        try {
+            Porcupine porcupine = new Porcupine("Porcupine", -1);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when numAnimals was negative", exceptionThrown);
+        exceptionThrown = false;
+
+        // Test with null name
+        try {
+            Porcupine porcupine = new Porcupine(null, 1);
+        } catch (IllegalArgumentException e) {
+        exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when name was null", exceptionThrown);
+        exceptionThrown = false;
+    }
+
+    /*Coyote constructor test for illegalargumentexception*/
+    @Test
+    public void testCoyoteConstructorInvalidData() {
+        boolean exceptionThrown = false;
+
+        // Test with negative numAnimals
+        try {
+            Coyote coyote = new Coyote("Coyote", -1);
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when numAnimals was negative", exceptionThrown);
+        exceptionThrown = false;
+
+        // Test with null name
+        try {
+            Coyote coyote = new Coyote(null, 1);
+        } catch (IllegalArgumentException e) {
+        exceptionThrown = true;
+        }
+        assertTrue("IllegalArgumentException was not thrown when name was null", exceptionThrown);
+        exceptionThrown = false;
+    }
+
+
+
+
 }
+
