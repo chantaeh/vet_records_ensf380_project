799a8dc223a8d36c7442e21d417b17f2f7469f7d
chantaeh <chantae.ho@ucalgary.ca>
2023-03-26 11:19:10 -0600

    Added member var and getter method

--- edu/ucalgary/oop/Crepuscular.java
+++ edu/ucalgary/oop/Crepuscular.java
@@ -1,5 +1,9 @@
 package edu.ucalgary.oop;
 
 public class Crepuscular {
-    
+    private static final int[] FEEDING_HOURS = {8, 9, 10};
+
+    public static int[] getFeedingHours() {
+        return FEEDING_HOURS;
+    }
 }
--- edu/ucalgary/oop/Diurnal.java
+++ edu/ucalgary/oop/Diurnal.java
@@ -1,5 +1,9 @@
 package edu.ucalgary.oop;
 
 public class Diurnal {
-    
+    private static final int[] FEEDING_HOURS = {19, 20, 21};
+
+    public static int[] getFeedingHours() {
+        return FEEDING_HOURS;
+    }
 }
