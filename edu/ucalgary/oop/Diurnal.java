a7eae70efd0fb250c82dfa58d19e9de0b225eb94
chantaeh <chantae.ho@ucalgary.ca>
2023-03-26 11:22:52 -0600

    Added documentation

--- edu/ucalgary/oop/Diurnal.java
+++ edu/ucalgary/oop/Diurnal.java
@@ -1,5 +1,11 @@
 package edu.ucalgary.oop;
 
+/**
+ * Abstract class for Diurnal animals. Gives int array of feeding hours.
+ * @author chantaeh 
+ * @version 1.0
+ * @since 1.0
+ */
 public class Diurnal extends Animal{
     private static final int[] FEEDING_HOURS = {19, 20, 21};
 
--- edu/ucalgary/oop/Nocturnal.java
+++ edu/ucalgary/oop/Nocturnal.java
@@ -1,5 +1,11 @@
 package edu.ucalgary.oop;
 
+/**
+ * Abstract class for Nocturnal animals. Gives int array of feeding hours.
+ * @author chantaeh 
+ * @version 1.0
+ * @since 1.0
+ */
 abstract class Nocturnal extends Animal{
     private static final int[] FEEDING_HOURS = {0, 1, 2};
 
