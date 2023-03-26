67ee26e2a03a692654c183404d8cd3ceb0b89605
chantaeh <chantae.ho@ucalgary.ca>
2023-03-26 11:17:47 -0600

    Added member var and getter method

--- edu/ucalgary/oop/Nocturnal.java
+++ edu/ucalgary/oop/Nocturnal.java
@@ -1,5 +1,9 @@
 package edu.ucalgary.oop;
 
-public class Nocturnal {
-    
+abstract class Nocturnal extends Animal{
+    private static final int[] FEEDING_HOURS = {0, 1, 2};
+
+    public static int[] getFeedingHours() {
+        return FEEDING_HOURS;
+    }
 }
