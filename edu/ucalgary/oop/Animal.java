051f92452b78845d68eece9e7ccaa4613c424e80
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2023-03-28 10:30:02 -0600

    documntation added

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,8 +1,20 @@
 package edu.ucalgary.oop;
 
+/* 
+ * Animal class
+ *
+ */
+
 public class Animal {
     String animalNickname;
     int numAnimals;
+
+    /*
+     * Constructor for Animal class
+     * @param animalNickname
+     * @param numAnimals
+     * @throws IllegalArgumentException
+     */
 
     public Animal(String animalNickname, int numAnimals) throws IllegalArgumentException {
         if (numAnimals < 0) {
@@ -13,18 +25,36 @@ public class Animal {
         this.numAnimals = numAnimals;
     }
 
+    /*
+     * Getter for animalNickname
+     * @return animalNickname
+     */
+
     public String getAnimalNickname() {
         return animalNickname;
     }
+
+    /*
+     * Getter for numAnimals
+     * @return numAnimals
+     */
 
     public int getNumAnimals() {
         return numAnimals;
     }
 
+    /*
+     * Setter for animalNickname
+     * @param animalNickname
+     */
     public void setAnimalNickname(String animalNickname) {
         this.animalNickname = animalNickname;
     }
 
+    /*
+     * Setter for numAnimals
+     * @param numAnimals
+     */
     public void setNumAnimals(int numAnimals) {
         this.numAnimals = numAnimals;
     }
