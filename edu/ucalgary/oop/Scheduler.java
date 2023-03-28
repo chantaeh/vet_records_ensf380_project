67f9da9d01ecb496c2b1a2f99e6f858356ea6c49
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2023-03-28 17:10:45 -0600

    fixed: executeQuery

--- edu/ucalgary/oop/Scheduler.java
+++ edu/ucalgary/oop/Scheduler.java
@@ -8,11 +8,9 @@ public class Scheduler {
     private Connection dbConnect;
 
     public Scheduler() {
-        
     }
 
     public void createConnection(){
-                
         try{
             dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "username", "password");
         } 
@@ -21,34 +19,19 @@ public class Scheduler {
         } 
     }
 
-    public String selectTreatments() {
+    public String selectAnimals() {
         ResultSet results;
 
         try {                    
             Statement myStmt = dbConnect.createStatement();
-            // results = myStmt.executeQuery("SELECT * FROM ANIMALS");
-            // results = myStmt.executeQuery("SELECT * FROM TASKS");
-            results = myStmt.executeQuery("SELECT * FROM TREATMENTS");
-            
-            while (results.next()){
-                
-                // System.out.print(results.getString("AnimalID") + " ");
-                // System.out.print(results.getString("AnimalNickname") + " ");
-                // System.out.print(results.getString("AnimalSpecies") + " ");
-                // System.out.println("");
 
-                // System.out.print(results.getString("TaskID") + " ");
-                // System.out.print(results.getString("Description") + " ");
-                // System.out.print(results.getString("Duration") + " ");
-                // System.out.print(results.getString("MaxWindow") + " ");
-                // System.out.println("");
-                
-                System.out.print(results.getString("TreatmentID") + " ");
+            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
+            
+            while (results.next()) {
                 System.out.print(results.getString("AnimalID") + " ");
-                System.out.print(results.getString("TaskID") + " ");
-                System.out.print(results.getString("StartHour") + " ");
+                System.out.print(results.getString("AnimalNickname") + " ");
+                System.out.print(results.getString("AnimalSpecies") + " ");
                 System.out.println("");
-                
             }
             
             myStmt.close();
@@ -58,35 +41,81 @@ public class Scheduler {
         return "";
     }    
 
-    public static String getFormatted(ArrayList<ArrayList<Task>> overallTasks) {
-        String outputString = "";
+    public String selectTasks() {
+        ResultSet results;
 
-        outputString = "Schedule for " + LocalDate.now().toString() + "\n";
-
-        for (ArrayList<Task> hourlyTasks : overallTasks) {
-            if (hourlyTasks != null) {
-                outputString += String.valueof(hourlyTasks.get(0).getStartHour()) + ":00";
-
-                for (Task task : hourlyTasks) {
-                    if (task.getDescription() != "Feeding" && task.getDescription() != "Cage cleaning") {
-                        outputString += "* " + task.getDescription(); 
-                        outputString += "(" + task.getAnimal().getAnimalNickname() + ")";
-                    }
-                    else if (task.getDescription() == "Feeding") {
-                        outputString += "* " + task.getDescription();
-                        outputString += " - " + task.getclass().getName(); 
-                        outputString += " (" + task.getAnimal().getAnimalNickname() + ")";
-                    }
-                    else {
-                        outputString += "* " + task.getDescription();
-                        outputString += " - " + task.getclass().getName(); 
-                        outputString += " (" + task.getAnimal().getAnimalNickname() + ")";
-                    }
-                }
+        try {                    
+            Statement myStmt = dbConnect.createStatement();
+        
+            results = myStmt.executeQuery("SELECT * FROM TASKS");
+            
+            while (results.next()){
+                System.out.print(results.getString("TaskID") + " ");
+                System.out.print(results.getString("Description") + " ");
+                System.out.print(results.getString("Duration") + " ");
+                System.out.print(results.getString("MaxWindow") + " ");
+                System.out.println("");
             }
+            
+            myStmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-        return outputString;
-    }
+        return "";
+    }    
+
+    public String selectTreatments() {
+        ResultSet results;
+
+        try {                    
+            Statement myStmt = dbConnect.createStatement();
+    
+            results = myStmt.executeQuery("SELECT * FROM TREATMENTS");
+            
+            while (results.next()){
+                System.out.print(results.getString("TreatmentID") + " ");
+                System.out.print(results.getString("AnimalID") + " ");
+                System.out.print(results.getString("TaskID") + " ");
+                System.out.print(results.getString("StartHour") + " ");
+                System.out.println("");
+            }
+            
+            myStmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return "";
+    }    
+
+    // public static String getFormatted(ArrayList<ArrayList<Task>> overallTasks) {
+    //     String outputString = "";
+
+    //     outputString = "Schedule for " + LocalDate.now().toString() + "\n";
+
+    //     for (ArrayList<Task> hourlyTasks : overallTasks) {
+    //         if (hourlyTasks != null) {
+    //             outputString += String.valueof(hourlyTasks.get(0).getStartHour()) + ":00";
+
+    //             for (Task task : hourlyTasks) {
+    //                 if (task.getDescription() != "Feeding" && task.getDescription() != "Cage cleaning") {
+    //                     outputString += "* " + task.getDescription(); 
+    //                     outputString += "(" + task.getAnimal().getAnimalNickname() + ")";
+    //                 }
+    //                 else if (task.getDescription() == "Feeding") {
+    //                     outputString += "* " + task.getDescription();
+    //                     outputString += " - " + task.getclass().getName(); 
+    //                     outputString += " (" + task.getAnimal().getAnimalNickname() + ")";
+    //                 }
+    //                 else {
+    //                     outputString += "* " + task.getDescription();
+    //                     outputString += " - " + task.getclass().getName(); 
+    //                     outputString += " (" + task.getAnimal().getAnimalNickname() + ")";
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return outputString;
+    // }
 
     public static void main(String[] args) {
         
@@ -94,6 +123,8 @@ public class Scheduler {
 
         scheduler.createConnection();
 
+        String allAnimals = scheduler.selectAnimals();
+        String allTasks = scheduler.selectTasks();
         String allTreatments = scheduler.selectTreatments();
         
 
