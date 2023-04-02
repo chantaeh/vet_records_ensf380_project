fc8656cc423c1a24308b9f3706cbb9d67450e8ad
Andrew Yan <122841883+andrewy94@users.noreply.github.com>
2023-04-02 11:28:58 -0600

    edited schedulerGUI to run without db to test GUI itself

--- edu/ucalgary/oop/SchedulerGUI.java
+++ edu/ucalgary/oop/SchedulerGUI.java
@@ -1,33 +1,26 @@
+package edu.ucalgary.oop;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class schedulerGUI extends JFrame implements ActionListener {
-    private JButton generateBtn, printBtn, addTaskBtn;
+public class SchedulerGUI extends JFrame implements ActionListener {
+    private JButton generateBtn, printBtn, editTaskBtn;
     private JTextArea outputArea;
     private Connection connectDB;
 
-    public schedulerGUI() {
+    public SchedulerGUI() {
         super("EWR Scheduler");
-
-        generateBtn = new JButton("Generate Schedule");
-        printBtn = new JButton("Print to .txt File");
-        editTaskBtn = new JButton("Edit Tasks");
-        outputArea = new JTextArea(20, 50);
-
-        JPanel panel = new JPanel();
-        panel.add(generateBtn);
-        panel.add(printBtn);
-        panel.add(addTaskBtn);
-
-        add(panel, BorderLayout.NORTH);
-        add(new JScrollPane(outputArea), BorderLayout.CENTER);
+        setSize(600, 400);
+        setupGUI();
+        setVisible(true);
 
         generateBtn.addActionListener(this);
         printBtn.addActionListener(this);
-        addTaskBtn.addActionListener(this);
+        editTaskBtn.addActionListener(this);
 
+        /* commented out to test gui itself for now
         try {
             // connect to the database
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -36,27 +29,43 @@ public class schedulerGUI extends JFrame implements ActionListener {
             ex.printStackTrace();
             System.exit(1);
         }
-
-        setSize(600, 400);
+        */
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+
+    }
+
+    private void setupGUI() {
+        generateBtn = new JButton("Generate Schedule");
+        printBtn = new JButton("Print to .txt File");
+        editTaskBtn = new JButton("Edit Tasks");
+        outputArea = new JTextArea(20, 20);
+
+        JPanel panel = new JPanel();
+        panel.add(generateBtn);
+        panel.add(printBtn);
+        panel.add(editTaskBtn);
+
+        add(panel, BorderLayout.NORTH);
+        add(new JScrollPane(outputArea), BorderLayout.CENTER);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == generateBtn) {
-            // generate schedule
-            //if backup volunteers are need, generate pop up prompt to remind user to confirm/call the backup volunteer
-            outputArea.setText("Schedule generated!");
+             // generate schedule
+            //if backup volunteers are needed, generate pop up prompt to remind user to confirm/call the backup volunteer
+            outputArea.setText("Schedule Generated!");
         } else if (e.getSource() == printBtn) {
+
             // print schedule to .txt file
             outputArea.setText("Schedule printed to file!");
-        } else if (e.getSource() == addTaskBtn) {
+        } else if (e.getSource() == editTaskBtn) {
+
             // add/delete/move task in SQL database
             outputArea.setText("Tasks Edited!");
         }
     }
 
     public static void main(String[] args) {
-        new schedulerGUI();
+        new SchedulerGUI();
     }
 }
