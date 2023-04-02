c8d2f2ee4e321d1dc257484043755867da80fba2
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2023-04-01 22:33:18 -0600

    fixed Diurnal feeding hours

--- edu/ucalgary/oop/Diurnal.java
+++ edu/ucalgary/oop/Diurnal.java
@@ -7,7 +7,7 @@ package edu.ucalgary.oop;
  * @since 1.0
  */
 public class Diurnal extends Animal{
-    private static final int[] FEEDING_HOURS = {19, 20, 21};
+    private static final int[] FEEDING_HOURS = {8, 9, 10};
 
     public Diurnal(String nickname, int numAnimals) {
         super(nickname, numAnimals);
