029455c9ddaac260861c05e95af74399fcfa8fbe
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2023-04-04 14:46:41 -0600

    docs added to Nocturnal

--- edu/ucalgary/oop/Nocturnal.java
+++ edu/ucalgary/oop/Nocturnal.java
@@ -9,10 +9,19 @@ package edu.ucalgary.oop;
 abstract class Nocturnal extends Animal{
     private static final int FEED_START_HOUR = 0;
 
+    /**
+     * Constructor for Nocturnal class
+     * @param nickname
+     * @param numAnimals
+     */
     public Nocturnal(String nickname, int numAnimals) {
         super(nickname, numAnimals);
     }
 
+    /**
+     * Getter for FEED_START_HOUR
+     * @return FEED_START_HOUR
+     */
     public static int getFeedStartHour() {
         return FEED_START_HOUR;
     }
