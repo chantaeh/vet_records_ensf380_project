e44ed4fd44fe1f4124c59b116231bf542ccac513
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2023-04-04 14:46:01 -0600

    docs added to Fox

--- edu/ucalgary/oop/Fox.java
+++ edu/ucalgary/oop/Fox.java
@@ -13,18 +13,35 @@ package edu.ucalgary.oop;
     private static final int FEEDING_PREP_MINS = 5;
     private static final int CAGE_CLEAN_MINS = 5;
 
+    /**
+     * Constructor for Fox class
+     * @param name
+     * @param numAnimals
+     */
     public Fox(String name, int numAnimals) {
         super(name, numAnimals);
     }
 
+    /**
+     * Getter for FEED_MINS
+     * @return FEED_MINS
+     */
     public static int getFeedMins() {
         return FEED_MINS;
     }
 
+    /**
+     * Getter for FEEDING_PREP_MINS
+     * @return FEEDING_PREP_MINS
+     */
     public static int getFeedingPrepMins() {
         return FEEDING_PREP_MINS;
     }
 
+    /**
+     * Getter for CAGE_CLEAN_MINS
+     * @return CAGE_CLEAN_MINS
+     */
     public static int getCageCleanMins() {
         return CAGE_CLEAN_MINS;
     }
